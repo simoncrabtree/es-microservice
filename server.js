d76376f95aef91528ee11d9ec455e7bf478e5d88
Simon Crabtree <simon@crabsticks.net>
2016-01-29 13:55:52 +0000

    Test that handling an event increments the count in the info response

--- server.js
+++ server.js
@@ -2,11 +2,21 @@ var Hapi = require("hapi");
 var server = new Hapi.Server();
 server.connection({ port: 8989 });
 
+var _eventsHandled = 0;
+
 var info = function (request, reply) {
-  reply({version: require('./package.json').version});
+  reply({
+    version: require('./package.json').version,
+    eventsHandled: _eventsHandled
+  });
 };
 
 server.route({ method: 'GET', path: '/info', handler: info });
+
+server.handleEvent = function(type, stream, data, cb) {
+  _eventsHandled++;
+  cb(null);
+}
 
 server.start();
 console.log('hello server http://localhost:8989');
--- test/info.js
+++ test/info.js
@@ -4,7 +4,7 @@ var server = require("../server.js");
 var code = require("code");
 var serviceVersion = require("../package.json").version;
 
-lab.experiment("/", function() {
+lab.experiment("Info endpoint", function() {
   lab.test("GET /info returns version of service",
   function(done){
     var options = {method: "GET", url:"/info"};
@@ -12,6 +12,17 @@ lab.experiment("/", function() {
       var result=response.result;
       code.expect(result.version).to.equal(serviceVersion);
       done();
+    });
+  });
+
+  lab.test("Handling an event is reflected in the event stats response", function(done){
+    server.handleEvent("OrganisationCreated_v1", "org-123", {name: "Test Organisation 123"}, function(err) {
+      var options = {method: "GET", url:"/info"};
+      server.inject(options, function(response){
+        var result=response.result;
+        code.expect(result.eventsHandled).to.equal(1);
+        done();
+      });
     });
   });
 });
